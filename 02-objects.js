3efddcf4e6d4c2a55bfc715745b556a46825b687
Chett Tiller <chett.tiller@gmail.com>
2026-02-04 13:04:23 -0500

    move car obj to bottom of exercises

--- 02-objects.js
+++ 02-objects.js
@@ -72,6 +72,17 @@ const robot = {
 
 // 1. Create a new object `car` with these attributes: `make`, `model`, `gallonsInTank`, `maxGallons`, `currentMiles` (you may use the example below or make a new one...)
 
+// 2. Give the `car` a method `describeMakeAndModel` which returns the `make` and `model` for the car. For example: "Ford Taurus"
+
+// 3. Give the `car` a method `fillTank` which sets the `gallonsInTank` to the `maxGallons`
+
+// 4. Give the `car` a method `goForADrive` which sets the `gallonsInTank` to 5 less than it was and also sets `currentMiles` to be 100 more than it was
+
+// 5. Give the `car` a method `rewindMiles` which sets `currentMiles` to 0
+
+// the gauge for a car's mileage is called an odometer for those curious
+// rewinding the odometer is illegal btw
+
 const car = {
     make: "Ford",
     model: "Taurus",
@@ -95,14 +106,3 @@ const car = {
         this.currentMiles = 0
     }
 }
-
-// 2. Give the `car` a method `describeMakeAndModel` which returns the `make` and `model` for the car. For example: "Ford Taurus"
-
-// 3. Give the `car` a method `fillTank` which sets the `gallonsInTank` to the `maxGallons`
-
-// 4. Give the `car` a method `goForADrive` which sets the `gallonsInTank` to 5 less than it was and also sets `currentMiles` to be 100 more than it was
-
-// 5. Give the `car` a method `rewindMiles` which sets `currentMiles` to 0
-
-// the gauge for a car's mileage is called an odometer for those curious
-// rewinding the odometer is illegal btw
